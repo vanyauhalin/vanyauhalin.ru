e67c0ed86546bcd53ea537b8da329bba353f445e
vanyauhalin <vanyauhalin@gmail.com>
2021-09-22 20:35:47 +0300

    Правит watch

--- workspace/tasks/watch.js
+++ workspace/tasks/watch.js
@@ -6,7 +6,7 @@ import { FS_CONTENT } from '../constants/fs';
 export const watch = () => {
   [
     [FS_CONTENT.SRC_ASSETS_FONTS, fonts],
-    [FS_CONTENT.SRC_ASSETS_DEEP_STYLES, styles],
+    [[FS_CONTENT.SRC_ASSETS_STYLES, FS_CONTENT.SRC_ASSETS_DEEP_STYLES], styles],
   ].forEach(([content, task]) => {
     gulp.watch(content, gulp.parallel(task));
   });
